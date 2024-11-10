e0c3e809b82e01508bc343566bd8801b353b2252
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-10 13:14:37 +0530

    All code add 13th time commit

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,41 +1,242 @@
+// import { Webhook } from "svix";
+// import { headers } from "next/headers";
+// import { WebhookEvent } from "@clerk/nextjs/server";
+// import { ClerkUser, createOrUpdateUser } from "@/actions/user";
+
+// export async function POST(req: Request) {
+//   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+//   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+//   if (!WEBHOOK_SECRET) {
+//     throw new Error(
+//       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+//     );
+//   }
+
+//   // Get the headers
+//   const headerPayload = await headers();
+//   const svix_id = headerPayload.get("svix-id");
+//   const svix_timestamp = headerPayload.get("svix-timestamp");
+//   const svix_signature = headerPayload.get("svix-signature");
+
+//   // If there are no headers, error out
+//   if (!svix_id || !svix_timestamp || !svix_signature) {
+//     return new Response("Error occured -- no svix headers", {
+//       status: 400,
+//     });
+//   }
+
+//   // Get the body
+//   const payload = await req.json();
+//   const body = JSON.stringify(payload);
+
+//   // Create a new Svix instance with your secret.
+//   const wh = new Webhook(WEBHOOK_SECRET);
+
+//   let evt: WebhookEvent;
+
+//   // Verify the payload with the headers
+//   try {
+//     evt = wh.verify(body, {
+//       "svix-id": svix_id,
+//       "svix-timestamp": svix_timestamp,
+//       "svix-signature": svix_signature,
+//     }) as WebhookEvent;
+//   } catch (err) {
+//     console.error("Error verifying webhook:", err);
+//     return new Response("Error occured", {
+//       status: 400,
+//     });
+//   }
+
+//   // Do something with the payload
+//   // For this guide, you simply log the payload to the console
+//   const { id } = evt.data;
+//   const eventType = evt.type;
+//   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+//   console.log("Webhook body:", body);
+
+//   if (eventType === "user.created" || eventType === "user.updated") {
+//     console.log("evt Data=", evt.data);
+
+//     const userData = evt.data as unknown as {
+//       id: string;
+//       first_name: string;
+//       last_name: string;
+//       image_url: string;
+//       email_addresses: { email: string }[];
+//       username: string;
+//     };
+
+//     const userProps: ClerkUser = {
+//       id: userData.id,
+//       first_name: evt.data.unsafe_metadata.first_name,
+//       last_name: evt.data.unsafe_metadata.last_name,
+//       image_url: userData.image_url,
+//       email_addresses: userData.email_addresses,
+//       username: userData.username,
+//     };
+//     console.log("userProps=", userProps);
+//     try {
+//       const newUser = await createOrUpdateUser(userProps);
+//       console.log("newUser=", newUser);
+
+//       return Response.json(
+//         {
+//           message: "User created or updated",
+//           user: newUser,
+//         },
+//         { status: 200 }
+//       );
+//     } catch (error) {
+//       console.error("Error creating or updating user:", error);
+//       return Response.json(
+//         { message: "Error creating or updating user" },
+//         {
+//           status: 500,
+//         }
+//       );
+//     }
+//   }
+
+//   return new Response("", { status: 200 });
+// }
+
+// import { Webhook } from "svix";
+// import { headers } from "next/headers";
+// import { WebhookEvent } from "@clerk/nextjs/server";
+// import { ClerkUser, createOrUpdateUser } from "@/actions/user";
+
+// export async function POST(req: Request) {
+//   // Retrieve the WEBHOOK_SECRET from environment variables
+//   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+//   if (!WEBHOOK_SECRET) {
+//     throw new Error(
+//       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+//     );
+//   }
+
+//   // Access headers directly (no need to await)
+//   const headerPayload = headers();
+//   const svix_id = (await headerPayload).get("svix-id");
+//   const svix_timestamp = (await headerPayload).get("svix-timestamp");
+//   const svix_signature = (await headerPayload).get("svix-signature");
+
+//   // Check for required Svix headers
+//   if (!svix_id || !svix_timestamp || !svix_signature) {
+//     return new Response("Missing Svix headers", { status: 400 });
+//   }
+
+//   // Parse the request body
+//   const payload = await req.json();
+//   const body = JSON.stringify(payload);
+
+//   // Initialize Svix webhook instance
+//   const wh = new Webhook(WEBHOOK_SECRET);
+//   let evt: WebhookEvent;
+
+//   // Verify the payload
+//   try {
+//     evt = wh.verify(body, {
+//       "svix-id": svix_id,
+//       "svix-timestamp": svix_timestamp,
+//       "svix-signature": svix_signature,
+//     }) as WebhookEvent;
+//   } catch (err) {
+//     console.error("Error verifying webhook:", err);
+//     return new Response("Verification error", { status: 400 });
+//   }
+
+//   // Process event data
+//   const eventType = evt.type;
+//   console.log(
+//     `Received webhook with ID: ${evt.data.id} and type: ${eventType}`
+//   );
+
+//   if (eventType === "user.created" || eventType === "user.updated") {
+//     console.log("Event data:", evt.data);
+
+//     const userData = evt.data as {
+//       id: string;
+//       first_name: string;
+//       last_name: string;
+//       image_url: string;
+//       email_addresses: { email: string }[];
+//       username: string;
+//       unsafe_metadata?: {
+//         first_name?: string;
+//         last_name?: string;
+//       };
+//     };
+
+//     const userProps: ClerkUser = {
+//       id: userData.id,
+//       first_name: userData.unsafe_metadata?.first_name || userData.first_name,
+//       last_name: userData.unsafe_metadata?.last_name || userData.last_name,
+//       image_url: userData.image_url,
+//       email_addresses: userData.email_addresses,
+//       username: userData.username,
+//     };
+//     console.log("User properties:", userProps);
+
+//     try {
+//       const newUser = await createOrUpdateUser(userProps);
+//       console.log("User created or updated:", newUser);
+
+//       return new Response(
+//         JSON.stringify({
+//           message: "User created or updated",
+//           user: newUser,
+//         }),
+//         { status: 200, headers: { "Content-Type": "application/json" } }
+//       );
+//     } catch (error) {
+//       console.error("Error creating or updating user:", error);
+//       return new Response(
+//         JSON.stringify({ message: "Error creating or updating user" }),
+//         { status: 500, headers: { "Content-Type": "application/json" } }
+//       );
+//     }
+//   }
+
+//   // Respond to non-user creation/update events
+//   return new Response("", { status: 200 });
+// }
+
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { ClerkUser, createOrUpdateUser } from "@/actions/user";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  // Retrieve the WEBHOOK_SECRET from environment variables
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
-  // Get the headers
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  // Access headers directly
+  const headerPayload = headers();
+  const svix_id = (await headerPayload).get("svix-id");
+  const svix_timestamp = (await headerPayload).get("svix-timestamp");
+  const svix_signature = (await headerPayload).get("svix-signature");
 
-  // If there are no headers, error out
+  // Check for required Svix headers
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+    return new Response("Missing Svix headers", { status: 400 });
   }
 
-  // Get the body
+  // Parse the request body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Initialize Svix webhook instance
   const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
+  // Verify the payload
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -44,60 +245,66 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    return new Response("Verification error", { status: 400 });
   }
 
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
+  // Process event data
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  console.log(
+    `Received webhook with ID: ${evt.data.id} and type: ${eventType}`
+  );
 
   if (eventType === "user.created" || eventType === "user.updated") {
-    console.log("evt Data=", evt.data);
+    console.log("Event data:", evt.data);
 
-    const userData = evt.data as unknown as {
+    // Define userData type based on Clerk's WebhookEvent structure
+    type UserData = {
       id: string;
-      first_name: string;
-      last_name: string;
-      image_url: string;
-      email_addresses: { email: string }[];
-      username: string;
+      first_name?: string;
+      last_name?: string;
+      image_url?: string;
+      email_addresses?: { email: string }[];
+      username?: string;
+      unsafe_metadata?: {
+        first_name?: string;
+        last_name?: string;
+      };
     };
+
+    const userData = evt.data as unknown as UserData;
 
     const userProps: ClerkUser = {
       id: userData.id,
-      first_name: userData.first_name,
-      last_name: userData.last_name,
-      image_url: userData.image_url,
-      email_addresses: userData.email_addresses,
-      username: userData.username,
+      first_name:
+        userData.unsafe_metadata?.first_name || userData.first_name || "",
+      last_name:
+        userData.unsafe_metadata?.last_name || userData.last_name || "",
+      image_url: userData.image_url || "",
+      email_addresses: userData.email_addresses || [],
+      username: userData.username || "",
     };
-    console.log("userProps=", userProps);
+    console.log("User properties:", userProps);
+
     try {
       const newUser = await createOrUpdateUser(userProps);
-      console.log("newUser=", newUser);
+      console.log("User created or updated:", newUser);
 
-      return Response.json(
-        {
+      return new Response(
+        JSON.stringify({
           message: "User created or updated",
           user: newUser,
-        },
-        { status: 200 }
+        }),
+        { status: 200, headers: { "Content-Type": "application/json" } }
       );
     } catch (error) {
       console.error("Error creating or updating user:", error);
-      return Response.json(
-        { message: "Error creating or updating user" },
-        {
-          status: 500,
-        }
+      return new Response(
+        JSON.stringify({ message: "Error creating or updating user" }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
       );
     }
   }
 
+  // Respond to non-user creation/update events
   return new Response("", { status: 200 });
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,234 +1,3 @@
-// "use client";
-
-// import Link from "next/link";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import { useState } from "react";
-// import { useSignUp } from "@clerk/nextjs";
-// import { toast } from "sonner";
-// import { useRouter } from "next/navigation";
-
-// type FormData = {
-//   username: string;
-//   email: string;
-//   password: string;
-//   firstName: string;
-//   lastName: string;
-// };
-
-// type FormErrors = Partial<FormData>;
-
-// const Signup = () => {
-//   const { isLoaded, signUp, setActive } = useSignUp();
-//   const [pendingVerification, setPendingVerification] = useState(false);
-//   const [code, setCode] = useState("");
-//   const [isSubmitting, setIsSubmitting] = useState(false);
-//   const router = useRouter();
-
-//   const [formData, setFormData] = useState<FormData>({
-//     username: "",
-//     email: "",
-//     password: "",
-//     firstName: "",
-//     lastName: "",
-//   });
-//   const [errors, setErrors] = useState<FormErrors>({});
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setFormData((prev) => ({ ...prev, [name]: value }));
-//   };
-
-//   const handleSubmit = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     setErrors({});
-
-//     try {
-//       setIsSubmitting(true);
-
-//       // Create new user with Clerk
-//       await signUp?.create({
-//         username: formData.username,
-//         emailAddress: formData.email,
-//         password: formData.password,
-//       });
-
-//       await signUp?.update({
-//         unsafeMetadata: {
-//           firstName: formData.firstName,
-//           lastName: formData.lastName,
-//         },
-//       });
-
-//       await signUp?.prepareEmailAddressVerification({ strategy: "email_code" });
-//       setPendingVerification(true);
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//     } catch (error: any) {
-//       console.error("Sign-up error:", error);
-//       setErrors({
-//         ...errors,
-//         [error.path as keyof FormErrors]: error.message,
-//       });
-//       toast.error("Sign-up failed. Please check your input.");
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
-
-//   const onPressVerify = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     if (!isLoaded) return;
-
-//     setIsSubmitting(true);
-//     try {
-//       const completeSignup = await signUp.attemptEmailAddressVerification({
-//         code,
-//       });
-//       if (completeSignup?.status === "complete") {
-//         await setActive({ session: completeSignup.createdSessionId });
-//         toast.success("Verification successful");
-//         router.push("/");
-//       } else {
-//         toast.error("Verification failed. Please try again.");
-//       }
-//     } catch (error) {
-//       console.error("Verification error:", error);
-//       toast.error("Verification failed. Please try again.");
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
-
-//   return (
-//     <div className="flex h-screen w-full items-center justify-center px-4">
-//       <Card className="mx-auto max-w-sm">
-//         <CardHeader>
-//           <CardTitle className="text-2xl">Sign Up</CardTitle>
-//           <CardDescription>
-//             Enter your email below to sign up for your account
-//           </CardDescription>
-//         </CardHeader>
-//         <CardContent>
-//           {!pendingVerification ? (
-//             <form onSubmit={handleSubmit} className="grid gap-4">
-//               <div className="grid gap-2 grid-cols-2">
-//                 <div>
-//                   <Label htmlFor="firstName">First Name</Label>
-//                   <Input
-//                     id="firstName"
-//                     name="firstName"
-//                     type="text"
-//                     placeholder="First Name"
-//                     value={formData.firstName}
-//                     onChange={handleChange}
-//                   />
-//                   {errors.firstName && (
-//                     <div className="text-red-500 text-sm">
-//                       {errors.firstName}
-//                     </div>
-//                   )}
-//                 </div>
-//                 <div>
-//                   <Label htmlFor="lastName">Last Name</Label>
-//                   <Input
-//                     id="lastName"
-//                     name="lastName"
-//                     type="text"
-//                     placeholder="Last Name"
-//                     value={formData.lastName}
-//                     onChange={handleChange}
-//                   />
-//                   {errors.lastName && (
-//                     <div className="text-red-500 text-sm">
-//                       {errors.lastName}
-//                     </div>
-//                   )}
-//                 </div>
-//               </div>
-//               <div className="grid gap-2">
-//                 <Label htmlFor="username">Username</Label>
-//                 <Input
-//                   id="username"
-//                   name="username"
-//                   type="text"
-//                   placeholder="Username"
-//                   value={formData.username}
-//                   onChange={handleChange}
-//                 />
-//                 {errors.username && (
-//                   <div className="text-red-500 text-sm">{errors.username}</div>
-//                 )}
-//               </div>
-//               <div className="grid gap-2">
-//                 <Label htmlFor="email">Email</Label>
-//                 <Input
-//                   id="email"
-//                   name="email"
-//                   type="email"
-//                   placeholder="m@example.com"
-//                   value={formData.email}
-//                   onChange={handleChange}
-//                 />
-//                 {errors.email && (
-//                   <div className="text-red-500 text-sm">{errors.email}</div>
-//                 )}
-//               </div>
-//               <div className="grid gap-2">
-//                 <Label htmlFor="password">Password</Label>
-//                 <Input
-//                   id="password"
-//                   name="password"
-//                   type="password"
-//                   value={formData.password}
-//                   onChange={handleChange}
-//                 />
-//                 {errors.password && (
-//                   <div className="text-red-500 text-sm">{errors.password}</div>
-//                 )}
-//               </div>
-//               <Button type="submit" className="w-full" disabled={isSubmitting}>
-//                 {isSubmitting ? "Signing Up..." : "Sign Up"}
-//               </Button>
-//             </form>
-//           ) : (
-//             <form onSubmit={onPressVerify} className="space-y-4">
-//               <div className="space-y-2">
-//                 <Label htmlFor="code">Verification Code</Label>
-//                 <Input
-//                   id="code"
-//                   value={code}
-//                   onChange={(e) => setCode(e.target.value)}
-//                   placeholder="Enter verification code"
-//                   required
-//                 />
-//               </div>
-//               <Button type="submit" className="w-full" disabled={isSubmitting}>
-//                 {isSubmitting ? "Verifying..." : "Verify Email"}
-//               </Button>
-//             </form>
-//           )}
-//           <div className="mt-4 text-center text-sm">
-//             Already have an account?{" "}
-//             <Link href="/sign-in" className="underline">
-//               Sign In
-//             </Link>
-//           </div>
-//         </CardContent>
-//       </Card>
-//     </div>
-//   );
-// };
-
-// export default Signup;
-
 "use client";
 
 import Link from "next/link";
@@ -305,18 +74,22 @@ const Signup = () => {
 
       // Create new user with Clerk
       setIsSubmitting(true);
-      await signUp?.create({
+      const user = await signUp?.create({
         username: formData.username,
         emailAddress: formData.email,
         password: formData.password,
       });
 
-      await signUp?.update({
+      console.log("user=", user);
+
+      const user2 = await signUp?.update({
         unsafeMetadata: {
           firstName: formData.firstName,
           lastName: formData.lastName,
         },
       });
+
+      console.log("user2=", user2);
 
       await signUp?.prepareEmailAddressVerification({ strategy: "email_code" });
       setPendingVerification(true);
