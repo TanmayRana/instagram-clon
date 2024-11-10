b0bfd08d7b8018582002c27bc0a946db3d393a11
Tanmay Rana <tanmayrana2001@gmail.com>
2024-11-10 11:48:11 +0530

    All code add 5th time commit

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -503,6 +503,256 @@
 
 // import statements...
 
+// "use client";
+
+// import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// // import { useFormik } from "formik";
+// import { useFormik } from "formik";
+// import * as Yup from "yup";
+// import { useState } from "react";
+// import axios from "axios";
+// import { useSignUp } from "@clerk/nextjs";
+// import { toast } from "sonner";
+// import { useRouter } from "next/navigation";
+
+// // Form validation schema
+// const validationSchema = Yup.object({
+//   username: Yup.string()
+//     .matches(
+//       /^[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?/-]*$/,
+//       "Username can only contain letters, numbers, and special characters."
+//     )
+//     .min(4, "Username must be at least 4 characters long")
+//     .max(8, "Username must be at most 8 characters long")
+//     .required("Username is required"),
+
+//   email: Yup.string()
+//     .email("Invalid email address")
+//     .required("Email is required"),
+
+//   password: Yup.string().required("Password is required"),
+//   firstName: Yup.string().required("First Name is required"),
+//   lastName: Yup.string().required("Last Name is required"),
+// });
+
+// const Signup = () => {
+//   const { isLoaded, signUp, setActive } = useSignUp();
+//   const [pendingVerification, setPendingVerification] = useState(false);
+//   const [code, setCode] = useState("");
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   const router = useRouter();
+
+//   const formik = useFormik({
+//     initialValues: {
+//       username: "",
+//       email: "",
+//       password: "",
+//       firstName: "",
+//       lastName: "",
+//     },
+//     validationSchema,
+//     onSubmit: async (values) => {
+//       setIsSubmitting(true);
+//       try {
+//         // Check if username already exists
+//         const { data: users } = await axios.get("/api/getUser");
+
+//         const userExists = users.some(
+//           // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//           (user: any) => user.username === values.username
+//         );
+//         if (userExists) {
+//           toast.error("Username already exists");
+//           setIsSubmitting(false);
+//           return;
+//         }
+//         // Create new user with supported parameters
+//         await signUp?.create({
+//           username: values.username,
+//           emailAddress: values.email,
+//           password: values.password,
+//         });
+
+//         // Update user with metadata fields for firstName and lastName
+//         await signUp?.update({
+//           unsafeMetadata: {
+//             firstName: values.firstName,
+//             lastName: values.lastName,
+//           },
+//         });
+
+//         // Prepare email verification
+//         await signUp?.prepareEmailAddressVerification({
+//           strategy: "email_code",
+//         });
+//         setPendingVerification(true);
+//       } catch (err) {
+//         console.error(err);
+//         toast.error("Something went wrong. Please try again.");
+//       } finally {
+//         setIsSubmitting(false);
+//       }
+//     },
+//   });
+
+//   // Handle email verification
+//   const onPressVerify = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     if (!isLoaded) return;
+
+//     setIsSubmitting(true);
+//     try {
+//       const completeSignup = await signUp.attemptEmailAddressVerification({
+//         code,
+//       });
+//       if (completeSignup?.status === "complete") {
+//         await setActive({ session: completeSignup.createdSessionId });
+//         toast.success("Verification successful");
+//         router.push("/");
+//       } else {
+//         toast.error("Verification failed. Please try again.");
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       toast.error("Verification failed. Please try again.");
+//     } finally {
+//       setIsSubmitting(false);
+//     }
+//   };
+
+//   return (
+//     <div className="flex h-screen w-full items-center justify-center px-4">
+//       <Card className="mx-auto max-w-sm">
+//         <CardHeader>
+//           <CardTitle className="text-2xl">Sign Up</CardTitle>
+//           <CardDescription>
+//             Enter your email below to sign up to your account
+//           </CardDescription>
+//         </CardHeader>
+//         <CardContent>
+//           {!pendingVerification ? (
+//             <form onSubmit={formik.handleSubmit} className="grid gap-4">
+//               <div className="grid gap-2 grid-cols-2">
+//                 <div>
+//                   <Label htmlFor="firstName">First Name</Label>
+//                   <Input
+//                     id="firstName"
+//                     type="text"
+//                     placeholder="First Name"
+//                     {...formik.getFieldProps("firstName")}
+//                   />
+//                   {formik.touched.firstName && formik.errors.firstName && (
+//                     <div className="text-red-500 text-sm">
+//                       {formik.errors.firstName}
+//                     </div>
+//                   )}
+//                 </div>
+//                 <div>
+//                   <Label htmlFor="lastName">Last Name</Label>
+//                   <Input
+//                     id="lastName"
+//                     type="text"
+//                     placeholder="Last Name"
+//                     {...formik.getFieldProps("lastName")}
+//                   />
+//                   {formik.touched.lastName && formik.errors.lastName && (
+//                     <div className="text-red-500 text-sm">
+//                       {formik.errors.lastName}
+//                     </div>
+//                   )}
+//                 </div>
+//               </div>
+//               <div className="grid gap-2">
+//                 <Label htmlFor="username">Username</Label>
+//                 <Input
+//                   id="username"
+//                   type="text"
+//                   placeholder="Username"
+//                   {...formik.getFieldProps("username")}
+//                 />
+//                 {formik.touched.username && formik.errors.username && (
+//                   <div className="text-red-500 text-sm">
+//                     {formik.errors.username}
+//                   </div>
+//                 )}
+//               </div>
+
+//               <div className="grid gap-2">
+//                 <Label htmlFor="email">Email</Label>
+//                 <Input
+//                   id="email"
+//                   type="email"
+//                   placeholder="m@example.com"
+//                   {...formik.getFieldProps("email")}
+//                 />
+//                 {formik.touched.email && formik.errors.email && (
+//                   <div className="text-red-500 text-sm">
+//                     {formik.errors.email}
+//                   </div>
+//                 )}
+//               </div>
+
+//               <div className="grid gap-2">
+//                 <Label htmlFor="password">Password</Label>
+//                 <Input
+//                   id="password"
+//                   type="password"
+//                   {...formik.getFieldProps("password")}
+//                 />
+//                 {formik.touched.password && formik.errors.password && (
+//                   <div className="text-red-500 text-sm">
+//                     {formik.errors.password}
+//                   </div>
+//                 )}
+//               </div>
+
+//               <Button type="submit" className="w-full" disabled={isSubmitting}>
+//                 {isSubmitting ? "Signing Up..." : "Sign Up"}
+//               </Button>
+//             </form>
+//           ) : (
+//             <form onSubmit={onPressVerify} className="space-y-4">
+//               <div className="space-y-2">
+//                 <Label htmlFor="code">Verification Code</Label>
+//                 <Input
+//                   id="code"
+//                   value={code}
+//                   onChange={(e) => setCode(e.target.value)}
+//                   placeholder="Enter verification code"
+//                   required
+//                 />
+//               </div>
+
+//               <Button type="submit" className="w-full" disabled={isSubmitting}>
+//                 {isSubmitting ? "Verifying..." : "Verify Email"}
+//               </Button>
+//             </form>
+//           )}
+
+//           <div className="mt-4 text-center text-sm">
+//             Already have an account?{" "}
+//             <Link href="/sign-in" className="underline">
+//               Sign In
+//             </Link>
+//           </div>
+//         </CardContent>
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default Signup;
+
 "use client";
 
 import Link from "next/link";
@@ -516,14 +766,14 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-// import { useFormik } from "formik";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { useSignUp } from "@clerk/nextjs";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 // Form validation schema
 const validationSchema = Yup.object({
@@ -552,61 +802,60 @@ const Signup = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-      firstName: "",
-      lastName: "",
-    },
-    validationSchema,
-    onSubmit: async (values) => {
-      setIsSubmitting(true);
-      try {
-        // Check if username already exists
-        const { data: users } = await axios.get("/api/getUser");
-
-        const userExists = users.some(
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          (user: any) => user.username === values.username
-        );
-        if (userExists) {
-          toast.error("Username already exists");
-          setIsSubmitting(false);
-          return;
-        }
-        // Create new user with supported parameters
-        await signUp?.create({
-          username: values.username,
-          emailAddress: values.email,
-          password: values.password,
-        });
-
-        // Update user with metadata fields for firstName and lastName
-        await signUp?.update({
-          unsafeMetadata: {
-            firstName: values.firstName,
-            lastName: values.lastName,
-          },
-        });
-
-        // Prepare email verification
-        await signUp?.prepareEmailAddressVerification({
-          strategy: "email_code",
-        });
-        setPendingVerification(true);
-      } catch (err) {
-        console.error(err);
-        toast.error("Something went wrong. Please try again.");
-      } finally {
-        setIsSubmitting(false);
-      }
-    },
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = async (values: any) => {
+    setIsSubmitting(true);
+    try {
+      // Check if username already exists
+      // const { data: users } = await axios.get("/api/getUser");
+
+      // const userExists = users.some(
+      //   (user) => user.username === values.username
+      // );
+      // if (userExists) {
+      //   toast.error("Username already exists");
+      //   setIsSubmitting(false);
+      //   return;
+      // }
+      // Create new user with supported parameters
+      await signUp?.create({
+        username: values.username,
+        emailAddress: values.email,
+        password: values.password,
+      });
+
+      // Update user with metadata fields for firstName and lastName
+      await signUp?.update({
+        unsafeMetadata: {
+          firstName: values.firstName,
+          lastName: values.lastName,
+        },
+      });
+
+      // Prepare email verification
+      await signUp?.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+      setPendingVerification(true);
+    } catch (err) {
+      console.error(err);
+      toast.error("Something went wrong. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   // Handle email verification
-  const onPressVerify = async (e: React.FormEvent) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onPressVerify = async (e: any) => {
     e.preventDefault();
     if (!isLoaded) return;
 
@@ -641,7 +890,7 @@ const Signup = () => {
         </CardHeader>
         <CardContent>
           {!pendingVerification ? (
-            <form onSubmit={formik.handleSubmit} className="grid gap-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
               <div className="grid gap-2 grid-cols-2">
                 <div>
                   <Label htmlFor="firstName">First Name</Label>
@@ -649,11 +898,11 @@ const Signup = () => {
                     id="firstName"
                     type="text"
                     placeholder="First Name"
-                    {...formik.getFieldProps("firstName")}
+                    {...register("firstName")}
                   />
-                  {formik.touched.firstName && formik.errors.firstName && (
+                  {errors.firstName && (
                     <div className="text-red-500 text-sm">
-                      {formik.errors.firstName}
+                      {errors.firstName.message}
                     </div>
                   )}
                 </div>
@@ -663,11 +912,11 @@ const Signup = () => {
                     id="lastName"
                     type="text"
                     placeholder="Last Name"
-                    {...formik.getFieldProps("lastName")}
+                    {...register("lastName")}
                   />
-                  {formik.touched.lastName && formik.errors.lastName && (
+                  {errors.lastName && (
                     <div className="text-red-500 text-sm">
-                      {formik.errors.lastName}
+                      {errors.lastName.message}
                     </div>
                   )}
                 </div>
@@ -678,11 +927,11 @@ const Signup = () => {
                   id="username"
                   type="text"
                   placeholder="Username"
-                  {...formik.getFieldProps("username")}
+                  {...register("username")}
                 />
-                {formik.touched.username && formik.errors.username && (
+                {errors.username && (
                   <div className="text-red-500 text-sm">
-                    {formik.errors.username}
+                    {errors.username.message}
                   </div>
                 )}
               </div>
@@ -693,11 +942,11 @@ const Signup = () => {
                   id="email"
                   type="email"
                   placeholder="m@example.com"
-                  {...formik.getFieldProps("email")}
+                  {...register("email")}
                 />
-                {formik.touched.email && formik.errors.email && (
+                {errors.email && (
                   <div className="text-red-500 text-sm">
-                    {formik.errors.email}
+                    {errors.email.message}
                   </div>
                 )}
               </div>
@@ -707,11 +956,11 @@ const Signup = () => {
                 <Input
                   id="password"
                   type="password"
-                  {...formik.getFieldProps("password")}
+                  {...register("password")}
                 />
-                {formik.touched.password && formik.errors.password && (
+                {errors.password && (
                   <div className="text-red-500 text-sm">
-                    {formik.errors.password}
+                    {errors.password.message}
                   </div>
                 )}
               </div>
